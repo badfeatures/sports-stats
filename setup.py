3da4a8b909efcde17df10a3c5a13d5d27f0047e2
pirateandy <pirateandy@gmail.com>
2015-04-10 14:36:56 -0700

    Modified setup

--- setup.py
+++ setup.py
@@ -64,8 +64,10 @@ setup(
     long_description=get_readme(),
     author=__author__,
     url='https://github.com/sportsy/sports-stats',
-    packages=('sportsstats',),
-    package_data={'sportsstats': [__version__]},
+    packages=['sportsstats'],
+    package_data={
+        'sportsstats': [__version__]
+    }
     install_requires=install_requires,
     dependency_links=['https://github.com/kennethreitz/requests/tarball/master#egg=requests==2.6.0'],
     test_suite='nose.collector',
