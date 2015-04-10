305eb610b4509aa729f42c11d8b491fb318784f5
pirateandy <pirateandy@gmail.com>
2015-04-10 15:30:37 -0700

    modified init

--- sportsstats/__init__.py
+++ sportsstats/__init__.py
@@ -1,8 +1,8 @@
-__title__ = 'Stats LLC API Wrapper'
-__version__ = '0.0.1'
-__author__ = 'Sportsy, Inc.'
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2015 Sportsy, Inc.'
+from .api import StatsAPI
+from .api import StatsResponse
+from .exceptions import StatsConnectionError
+from .exceptions import StatsError
+from .exceptions import StatsRequestError
 
 import logging
 
@@ -10,17 +10,5 @@ import logging
 # No logging unless the client provides a handler,
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-try:
-    from .api import StatsAPI, StatsResponse
-    from .exceptions import StatsConnectionError, StatsError, StatsRequestError
-except:
-    pass
-
-
-__all__ = [
-    'StatsAPI',
-    'StatsResponse',
-    'StatsConnectionError',
-    'StatsError',
-    'StatsRequestError'
-]
+__title__ = 'sportsstats'
+__author__ = 'Sportsy, Inc.'
